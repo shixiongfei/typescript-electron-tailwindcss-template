1c9b8088e412c5e43fbbaeb191f6a79fb42d6268
shixiongfei <xiongfei.shi@icloud.com>
2023-05-28 20:58:30 +0800

    Remove quotation marks

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,3 +1,3 @@
-const Home = () => <div className="m-4">"TypeScript Electron App Starter"</div>;
+const Home = () => <div className="m-4">TypeScript Electron App Starter</div>;
 
 export default Home;
